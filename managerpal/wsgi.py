79085bc10b73ba96a18df9148070e31a5c733fec
Chow Yuan Jing <zcyuanjing@gmail.com>
2023-06-11 15:29:06 +0800

    Feat: Removed init_db, not required since flask-sqlalchemy handles the connection pooling

--- managerpal/wsgi.py
+++ managerpal/wsgi.py
@@ -1,3 +1,4 @@
+import os
 from appcore.app import create_app
 from appcore.db import db
 
@@ -5,4 +6,4 @@ if __name__ == "__main__":
     app = create_app()
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=os.environ.get("is_dev", False))
